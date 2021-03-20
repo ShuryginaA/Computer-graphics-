06b8d6e6e3fb9a006026e77613ea2849961106ad
GordeevVV <61159998+GordeevVV@users.noreply.github.com>
2021-03-20 23:56:30 +0300

    added all MathMorf filters to Form1

--- Form1.cs
+++ Form1.cs
@@ -108,5 +108,101 @@ namespace img_processing
             Filters filter = new SobelFilter();
             backgroundWorker1.RunWorkerAsync(filter);
         }
+        private void dirationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool[,] mask = new bool[3, 3];
+            mask[0, 0] = false;
+            mask[0, 1] = true;
+            mask[0, 2] = false;
+            mask[1, 0] = true;
+            mask[1, 1] = true;
+            mask[1, 2] = true;
+            mask[2, 0] = false;
+            mask[2, 1] = true;
+            mask[2, 2] = false;        
+            MathMorf dil = new Dilation();
+            //backgroundWorker2.RunWorkerAsync(dil);
+            Bitmap resultImage = dil.procImg(image,mask,3, 3/*,backgroundWorker2*/);
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+        }
+
+        private void erosionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool[,] mask = new bool[3, 3];
+            mask[0, 0] = false;
+            mask[0, 1] = true;
+            mask[0, 2] = false;
+            mask[1, 0] = true;
+            mask[1, 1] = true;
+            mask[1, 2] = true;
+            mask[2, 0] = false;
+            mask[2, 1] = true;
+            mask[2, 2] = false;
+            MathMorf eros = new Erosion();
+            // backgroundWorker2.RunWorkerAsync(eros);
+            Bitmap resultImage = eros.procImg(image, mask, 3, 3/*, backgroundWorker2*/);
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+        }
+
+        private void openingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool[,] mask = new bool[3, 3];
+            mask[0, 0] = true;
+            mask[0, 1] = true;
+            mask[0, 2] = true;
+            mask[1, 0] = true;
+            mask[1, 1] = true;
+            mask[1, 2] = true;
+            mask[2, 0] = true;
+            mask[2, 1] = true;
+            mask[2, 2] = true;
+            MathMorf open = new Opening();
+             //backgroundWorker2.RunWorkerAsync(open);
+            Bitmap resultImage = open.procImg(image, mask, 3, 3/*, backgroundWorker2*/);
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+        }
+
+        private void closingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool[,] mask = new bool[3, 3];
+            mask[0, 0] = true;
+            mask[0, 1] = true;
+            mask[0, 2] = true;
+            mask[1, 0] = true;
+            mask[1, 1] = true;
+            mask[1, 2] = true;
+            mask[2, 0] = true;
+            mask[2, 1] = true;
+            mask[2, 2] = true;
+            MathMorf close = new Closing();
+            //backgroundWorker2.RunWorkerAsync(close);
+            Bitmap resultImage = close.procImg(image, mask, 3, 3 /*backgroundWorker2*/);
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+
+        }
+
+        private void gradientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool[,] mask = new bool[3, 3];
+            mask[0, 0] = false;
+            mask[0, 1] = true;
+            mask[0, 2] = false;
+            mask[1, 0] = true;
+            mask[1, 1] = true;
+            mask[1, 2] = true;
+            mask[2, 0] = false;
+            mask[2, 1] = true;
+            mask[2, 2] = false;
+            MathMorf grad = new Gradient();
+            //backgroundWorker2.RunWorkerAsync(grad);
+            Bitmap resultImage = grad.procImg(image, mask, 3, 3/*, backgroundWorker2*/);
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+            //В разработке, фиксить пересечение потоков
+        }
     }
 }
