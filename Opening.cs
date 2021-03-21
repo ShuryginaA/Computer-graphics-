afac33347d495a4de4baf75058e1db434de3b3a8
GordeevVV <61159998+GordeevVV@users.noreply.github.com>
2021-03-21 17:39:19 +0300

    Added all MathMorfology (#5)
    * Add SobelFilter
    
    * Update Form1(Sobel)
    
    * Added MathMorfology filters
    
    * added all MathMorf filters to Form1
    
    Co-authored-by: ShuryginaA <60935207+ShuryginaA@users.noreply.github.com>

---
+++ Opening.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace CompGr1
+{
+    class Opening:MathMorf
+    {
+        public override Bitmap procImg(Bitmap sourceImage, bool[,] mask, int MW, int MH/*, BackgroundWorker worker*/)
+        {
+            MathMorf eros = new Erosion();
+            MathMorf dil = new Dilation();
+            Bitmap result = new Bitmap(sourceImage.Width, sourceImage.Height);
+            result =eros.procImg(sourceImage, mask, MW, MH/*,worker*/);
+            result = dil.procImg(result, mask, MW, MH/*,worker*/);
+            return result;
+        }
+    }
+}
